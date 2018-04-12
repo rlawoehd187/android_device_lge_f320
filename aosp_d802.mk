e01128407c17ea8d2453048668bffb7693146752
YoDevil <alessandro.astone@hotmail.com>
2018-04-12 17:56:34 +0000

    AEXify

--- aosp_d802.mk
+++ aosp_d802.mk
@@ -1,12 +1,12 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d802/d802.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d802
-PRODUCT_NAME := lineage_d802
+PRODUCT_NAME := aosp_d802
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D802
 PRODUCT_MANUFACTURER := lge
